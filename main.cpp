5b40dced1ffa961b727d9588d26a2778b864d226
yenark <67915882+yenark@users.noreply.github.com>
2021-07-19 12:52:13 -0300

    Tienda de celulares

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+#include<cstring>
+using namespace std;
+#include "funciones.h"
+#include "rlutil.h"
+#include "Vendedor.h"
+#include "conio.h"
+#include "celular.h"
+#include "menu.h"
+#include <windows.h>
+#include <locale.h>
+int main(){
+
+/*vendedor obj;
+obj.cargar();
+obj.guardarEnDisco();*/
+
+MENU();
+
+
+
+}
